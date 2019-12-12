10fcd545d21a98760e704b4df4905e508f2e0ce1
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-12 17:15:52 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,12 @@
 class Cat
-  attr_accessor :owner 
+  attr_accessor :owner, :mood 
   attr_reader :name 
   @@all = []
   
    def initialize (name, owner)
    @name = name 
-   self.owner = owner 
+   self.owner = owner
+   @mood = "nervous"
    @@all << self 
 end
 
