44ea4d961eccd95683727a8dceb14648236a71ac
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-12 17:07:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,20 +2,21 @@ class Owner
  attr_accessor :name, :species  
   @@all = []
   
- def initialize(name)
-   @name = name 
-   @species = "human"
-   @@all << self 
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self 
   end 
   
-def say_species 
-  "I am a #{@species}."
-end 
+  def say_species 
+    "I am a #{@species}."
+  end 
 
-def self. all 
-  @@all 
-end 
+  def self. all 
+    @@all 
+  end 
 
-def self.count
-  self.all.count 
+  def self.count
+    self.all.count 
+  end 
 end
